3ffbc46785c0bbfa43c9c87de81556275e1f231c
ViavaCos <13811092694@163.com>
2019-10-16 21:02:20 +0800

    only a test for cookie usage

---
+++ back/server.js
@@ -0,0 +1,106 @@
+// 引入express
+const express = require('express');
+// 引入body-parser
+const bodyParser = require('body-parser');
+// 引入cookie-parser 用于解析cookie
+const cookieParser = require('cookie-parser');
+
+
+// 创建服务
+const app = express();
+
+// 使用express4就引入了的body-parser包解析请求体中的数据
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// 使用cookie-parser
+app.use(cookieParser());
+
+
+
+
+// 方式二: 后端接口 2.CORS解决跨域问题
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     next();
+// })
+app.all('*', function (req, res, next) {
+    // console.log(`${Date.now()}:来自${req.connection.remoteAddress} 访问了 ${req.method}-${req.url}。参数是：${req.query},携带cookie:${req.headers.cookie}`);
+    res.header('Access-Control-Allow-Origin', req.headers.origin); //需要显示设置来源
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Credentials', true); //带cookies
+    next();
+});
+
+
+// 方式二： 后端接口 1.接口设置
+// 登录
+app.post('/login', (req, res) => {
+    // console.log(req.body);
+
+    let { username, password } = req.body;
+
+    if (username == 'admin' && password == 123) {
+        // 当请求成功时:  原生方法之设置cookie
+        // res.setHeader('set-cookie', 'username=admin');
+        // express自带方法设置cookie
+        res.cookie('username', 'admin');
+        res.send({
+            code: 200,
+            msg: '登录成功'
+        })
+    } else {
+        res.send({
+            code: 500,
+            msg: '登录失败！'
+        })
+    }
+})
+
+// 检查登录状态
+app.get('/checkLogin', (req, res, next) => {
+
+    console.log('------------------------------------------cookie----------------------------');
+    console.log(req.cookies);
+    // res.send('获取cookie成功');
+
+    let { username } = req.cookies;
+
+    console.log(username);
+
+    if (username === 'admin') {
+        res.send({ username, code: 200, msg: '已经登录' });
+    } else {
+        res.send({ code: 500, msg: '未登录' });
+    }
+})
+
+
+// 退出功能      bug: 退出之后点击返回，还是显示登录状态
+app.get('/logout', (req, res) => {
+    // 删除cookie
+    res.clearCookie('username');
+    res.send({ code: 200, msg: '退出成功！' });
+})
+
+
+// 方式一： 后端接口
+// app.get('/test', (req, res) => {
+
+//     let param = JSON.stringify(MSG);
+
+//     // res.send('fn(' + param + ');'); // 可行   
+//     res.send(`fn({code:200,data:${param},msg:'请求成功'});`);
+//     // 接收的参数必须是JSON字符串？
+
+// })
+
+
+
+
+// 监听端口
+app.listen(8080, () => {
+    console.log('8080: success!');
+})
